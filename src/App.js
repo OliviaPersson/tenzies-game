cdb493fd0bc4964af037337ebfc5d3775164e14b
Olivia Persson <olivia.persson@forefront.se>
2023-01-05 15:25:43 +0100

    Add dices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Dice from "./components/Dice";
 
 function App() {
   return (
     <main className="game-container">
-      <div className="game-board"></div>
+      <div className="game-board">
+        <div className="dice-container">
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+          <Dice />
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/Dice.jsx
@@ -0,0 +1,7 @@
+import "./Dice.css";
+
+function Dice() {
+  return <h1 className="dice">1</h1>;
+}
+
+export default Dice;
